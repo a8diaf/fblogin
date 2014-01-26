627abeeec2f8169f4fb49636b5724ac39bef538b
a8diaf <a8diaf@gmail.com>
2014-01-26 12:08:50 +0100

    like box

---
+++ fb-box.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+  
+
+
+</head>
+<body>
+
+
+
+
+
+
+  <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2FDzimmobilier.net&amp;width&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=648588785200007" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:290px;" allowTransparency="true"></iframe>
+</body>
+</html>
